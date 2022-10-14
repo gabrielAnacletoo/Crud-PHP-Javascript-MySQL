e4212511ee54f5d1a4b5554c59b360d1def10779
Gabriel Anacleto <109560707+gabrielAnacletoo@users.noreply.github.com>
2022-10-14 10:19:05 -0300

    Deletar Cadastro
    Deleta o cadastro apartir do ID que o mesmo tempo no BD

---
+++ delete.php
@@ -0,0 +1,66 @@
+<?php
+require 'banco.php';
+
+$id = 0;
+
+if(!empty($_GET['id']))
+{
+    $id = $_REQUEST['id'];
+}
+
+if(!empty($_POST))
+{
+    $id = $_POST['id'];
+
+    //Delete do banco:
+    $pdo = Banco::conectar();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "DELETE FROM pessoa where id = ?";
+    $q = $pdo->prepare($sql);
+    $q->execute(array($id));
+    Banco::desconectar();
+    header("Location: index.php");
+}
+?>
+
+    <!DOCTYPE html>
+    <html lang="pt-br">
+
+    <head>
+
+
+      <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+        <title>Deletar Contato</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="span10 offset1">
+                <div class="row">
+                    <h3 class="well"><font color="white">Excluir Cliente</font> </h3>
+                </div>
+                <form class="form-horizontal" action="delete.php" method="post">
+                    <input type="hidden" name="id" value="<?php echo $id;?>" />
+                    <div class="alert alert-danger"> Deseja excluir o cliente? 
+                    </div>
+                    <div class="form actions">
+                        <button type="submit" class="btn btn-danger">Sim</button>
+                        <a href="index.php" type="btn" class="btn btn-default"><font color="white">Não </font></a>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+
+    </html>
